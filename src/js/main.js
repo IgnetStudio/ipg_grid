d9056931f88cfc40e2b5787388eb954f921bd3b5
Patryk Stępień <patryk.stepien.0x58@gmail.com>
2025-02-13 21:40:46 +0100

    FIX2025: Build system rewrite (#61)
    * (environment): migrate to bun
    
    * (vite): remove legacy cruft
    
    * (dist): drop build artifact
    
    * reformat
    
    * (js): migrate to src
    
    * (pug): remove script
    
    * (assets): remove domain from path
    
    * (assets): migrate audio files
    
    * (style): properly import scss
    
    * (scss): remove circular dependencies
    
    * (js): extract text animation to separate file
    
    * (assets): migrate to proper location
    
    * (assets): migrate to proper location
    
    * (assets): remove domain from path
    
    * (public): migrate manifest
    
    * (assets): use absolute path
    
    * (dist): include other entry-points
    
    * (dist): include generated css in build result
    
    * (git): add dist to gitignore
    
    * (bun): normalize dependencies

---
+++ src/js/main.js
@@ -0,0 +1,98 @@
+// HTML elements
+import "../index.css";
+
+const body = document.body;
+const toggleBtn = document.querySelector(".toggle__btn");
+const tooltipBtns = document.querySelectorAll(".widget__info");
+const tooltipSpans = document.querySelectorAll(".widget__list-item");
+const contentInner = document.querySelector(".content__inner");
+
+// Overlay
+
+for (let i = 0; i < tooltipBtns.length; i++) {
+	tooltipBtns[i].addEventListener("mouseover", function () {
+		contentInner.classList.add("overlayed");
+	});
+
+	tooltipBtns[i].addEventListener("mouseout", function () {
+		contentInner.classList.remove("overlayed");
+	});
+}
+
+for (let i = 0; i < tooltipSpans.length; i++) {
+	tooltipSpans[i].addEventListener("mouseover", function () {
+		contentInner.classList.add("overlayed");
+	});
+
+	tooltipSpans[i].addEventListener("mouseout", function () {
+		contentInner.classList.remove("overlayed");
+	});
+}
+
+window.onload = function () {
+	const isDark = localStorage.getItem("darkMode");
+	if (isDark === "true") {
+		toggleDark();
+	}
+};
+
+const toggleDark = function () {
+	body.classList.toggle("dark-mode");
+};
+
+toggleBtn.addEventListener("click", function () {
+	const isDark = body.classList.contains("dark-mode");
+	toggleDark();
+	localStorage.setItem("darkMode", !isDark);
+	if (isDark) {
+		document.getElementById("switchOffAudio").play();
+	} else {
+		document.getElementById("switchOnAudio").play();
+	}
+});
+
+// Dark mode toggle, as per hour
+
+function isNight() {
+	const hour = new Date().getHours();
+	return hour > 22 || hour < 6;
+}
+
+if (isNight() === true) {
+	const isDark = localStorage.getItem("darkMode");
+	if (isDark === null) {
+		toggleDark();
+	}
+}
+
+// @LM img src error handler, will be utilized in the future
+async function checkImage(url) {
+	try {
+		// sends a request without downloading the whole image
+		const response = await fetch(url, { method: "HEAD" });
+
+		if (!response.ok) throw new Error("Image not found");
+		console.log("Image is valid and accessible");
+	} catch (error) {
+		console.log("Image is broken:", error.message);
+		// toast placeholder
+		// apply fallback styles to img with broken image icon
+	}
+}
+
+checkImage("assets/img/card/3H-thumbnail.png");
+
+// Scroll to top
+
+const folioTarget = document.querySelector("#folio");
+const cardTarget = document.querySelector(".card");
+const scrollTarget = document.querySelector(".footer__scroll-top button");
+
+// @LM buggy solution
+// cardTarget.addEventListener("click", function (e) {
+// 	folioTarget.scrollIntoView({ behavior: "smooth" });
+// });
+
+// scrollTarget.addEventListener("click", function (e) {
+// 	folioTarget.scrollIntoView({ behavior: "smooth" });
+// });
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
-export default {
-    server: {
-        watch: {
-            usePolling: true,
-        },
-        port: 3000,
-    },
-};
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+import pugPlugin from "vite-plugin-pug";
+
+const options = { pretty: true };
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				folio: resolve(__dirname, "folio.html"),
+				404: resolve(__dirname, "404.html"),
+			},
+		},
+	},
+	plugins: [pugPlugin(options)],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		port: 3000,
+	},
+});
+
