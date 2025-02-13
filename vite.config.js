55eb84c8c1834f9b64aa866b8625cf7a84515ea2
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-02-13 21:10:49 +0100

    (vite): remove legacy cruft

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-export default {
-    server: {
-        watch: {
-            usePolling: true,
-        },
-        port: 3000,
-    },
-};
+import { defineConfig } from "vite";
+import pugPlugin from "vite-plugin-pug";
+
+const options = { pretty: true };
+
+export default defineConfig({
+	plugins: [pugPlugin(options)],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		port: 3000,
+	},
+});
+
